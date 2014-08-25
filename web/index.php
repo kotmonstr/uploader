96313b5bf94627a823ede6cc125391df6ce6902c
kotmonstr <kotmonstr@ukr.net>
2014-08-26 00:10:15 +0400

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-
+ echo "Привет Дима!";var_dump(1);
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
